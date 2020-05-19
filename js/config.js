b0e621190094f53b5a6a0e98352b286973db9732
charmXccelerata <64551750+charmXccelerata@users.noreply.github.com>
2020-05-19 07:38:49 -0400

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: '<yours>', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '<yours>', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: '<yours>' // e.g. us-east-2
+    },
+    api: {
+      invokeUrl: '<yours>' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
